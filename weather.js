b5c71509176e8daf94e2f554f8665f37993ead84
garvshah07 <garv8890@gmail.com>
2024-01-26 15:12:14 +0530

    commit intial

--- weather.js
+++ weather.js
@@ -1,13 +1,14 @@
-// Node Div
+// import { dot } from "node:test/reporters";
 
+// Node Div
 const mainDiv = document.querySelector(".main-div");
 
 // Input area and search button
-
 const searchDiv = document.querySelector(".search-div");
 
-// Create input Element
+const apikey = "a2fc58f7ff00d6e3b9510c8d39486d4d";
 
+// Create input Element
 const inputText = document.createElement("input");
 inputText.id = "input-text";
 inputText.type = "text";
@@ -15,7 +16,6 @@ inputText.placeholder = "Enter city name...";
 inputText.value = "";
 
 // Create Search button Element
-
 const searchBtn = document.createElement("button");
 searchBtn.innerHTML = "Search";
 searchBtn.id = "search-btn";
@@ -24,55 +24,111 @@ searchBtn.id = "search-btn";
 searchDiv.appendChild(inputText);
 searchDiv.appendChild(searchBtn);
 
-const city = (inputText.value = "Ahmedabad");
+// Function to get current time
+const getCurrentTime = () => {
+  const date = new Date();
 
-// fetch function with api
+  const hour = date.getHours().toString() % 12 || 12;
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+  const period = hour >= 12 ? "PM" : "AM";
+  return `${hour} : ${minute} : ${second} ${period}`;
+};
 
-const getWeather = async (city) => {
-  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${inputText.value}`;
+const getCurrentDay = () => {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const date = new Date();
+  const dayOfWeek = daysOfWeek[date.getDay()];
 
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "1b5f18be7emsh5bf624e16015622p1d05a2jsn36a9f8f4bb6a",
-      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-    },
-  };
+  return `${dayOfWeek}`;
+};
 
+// Function to get weather information
+const getWeather = async () => {
   try {
-    if (inputText.value == "") {
-      return;
-    } else {
-      const response = await fetch(url, options);
-      const data = await response.json();
+    let cityName = inputText.value;
 
-      const searchDetail = document.querySelector(".search-detail-div");
-      searchDetail.innerHTML = "";
-      const content = `
-    <div>
-       <h1>Weather of <span>${inputText.value || "ahmedabad"}</span> </h1>
-       </br>
-       <h2>Temprature : ${data.temp || "0"}&#8451</h2>
-    <div>
-    </br>
-    <div>
-      <h2>Wind : ${data.wind_speed || "0"} Km/H </h2>
-    </div>
+    // If input is empty, set default city to Ahmedabad
+    if (cityName === "") {
+      cityName = "Ahmedabad";
+    }
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apikey}`
+    );
+
+    const data = await response.json();
+    console.log(data);
+    const iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    const searchDetail = document.querySelector(".search-detail-div");
+    searchDetail.innerHTML = "";
+
+    const time = getCurrentTime();
+    const day = getCurrentDay();
+    const { icon } = data.weather[0];
+    const content = `
+      <div class="card">
+            <div class="timezone">
+              <h4>${time}</h4>
+              <h4>${day}</h4>
+            </div>
+          <div class="name">
+          <h3><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" class="text-2xl" height="17px" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"></path></svg>${
+            data.name
+          }, <span>${data.sys.country}</span></h3>
+        </div>
+    <div class="temp">
+          <h3>Temperature: ${Math.round(data.main.temp) || "0"}°</h3>
+          <div class="temp-inner">
+              <div>
+                 <h6>H: ${data.main.temp_max || 0}</h6>
+                 <h6>L: ${data.main.temp_min || 0}</h6>
+              </div>
+         <div class="img-inner">
+                <img class="img"  src="${iconUrl}" alt="Weather Icon" />
+                <h6>${data.weather[0].description}</h6>
+         </div>
+     </div>
+        </div>
+      </div>
     `;
 
-      searchDetail.insertAdjacentHTML("afterbegin", content);
+    searchDetail.insertAdjacentHTML("afterbegin", content);
 
-      // inputText.value = "";
-    }
+    // Clear the input field only if a city was entered (not the default)
+
+    // if (inputText.value !== "") {
+    //   inputText.value = "";
+    // }
   } catch (error) {
     console.error(error);
   }
 };
 
+// Initial call to getWeather
 getWeather();
 
-// Search button Handler
+// Set interval to update time every second
+setInterval(() => {
+  const timezoneDiv = document.querySelector(".timezone");
+  if (timezoneDiv) {
+    timezoneDiv.querySelectorAll("h4").textContent = getCurrentTime();
+  }
+  if (timezoneDiv) {
+    timezoneDiv.querySelectorAll("h4").textContent = getCurrentDay();
+  }
+}, 1000);
 
+// Search button Handler
 searchBtn.addEventListener("click", (event) => {
   event.preventDefault();
   getWeather();
